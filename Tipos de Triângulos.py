2f2f560e92a4fb592b1c95d482d52e8beab6bd2a
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-18 20:28:17 -0300

    Update Tipos de Triângulos.py

--- Tipos de Triângulos.py
+++ Tipos de Triângulos.py
@@ -1,3 +1,11 @@
+"""Define a classe Triangulo cujo construtor recebe 3 valores 
+inteiros correspondentes aos lados a, b e c de um triângulo.
+A classe triângulo também possui um método perimetro, que não 
+recebe parâmetros e devolve um valor inteiro correspondente ao perímetro do triângulo.
+Possui, também, um método tipo_lado, que não recebe parâmetros
+e devolve uma string dizendo se o triângulo é isósceles (dois lados iguais),
+equilátero (todos os lados iguais) e escaleno (todos os lados diferentes) """
+
 class Triangulo():
     def __init__(self,a,b,c):
         self.a = a
