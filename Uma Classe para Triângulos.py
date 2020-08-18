b2c922a7d6b9184aac1ba20d87c8c680f23957d8
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-18 20:11:59 -0300

    Update Uma Classe para Triângulos.py

--- Uma Classe para Triângulos.py
+++ Uma Classe para Triângulos.py
@@ -1,3 +1,9 @@
+"""Define a classe Triangulo cujo construtor recebe 3 valores 
+inteiros correspondentes aos lados a, b e c de um triângulo.
+A classe triângulo também possui um método perimetro, que não 
+recebe parâmetros e devolve um valor inteiro correspondente ao perímetro do triângulo."""
+
+
 class Triangulo():
     def __init__(self,a,b,c):
         self.a = a
