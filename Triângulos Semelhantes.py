a3129f47fffd74ede539ba1b35382305dcfa7519
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-19 13:21:41 -0300

    Add files via upload

---
+++ Triângulos Semelhantes.py
@@ -0,0 +1,32 @@
+class Triangulo():
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimetro(self):
+        return self.a + self.b + self.c
+
+    def tipo_lado(self):
+        if self.a != self.b and self.a != self.c and self.b != self.c:
+            return "escaleno"
+        elif self.a == self.b and self.a == self.c and self.b == self.c:
+            return "equilátero"
+        else:
+            return "isósceles"
+
+    def retangulo(self):
+        if (self.a ** 2) == (self.b ** 2) + (self.c ** 2) or (self.b ** 2) == (self.a ** 2) + (self.c ** 2) or (self.c ** 2) == (self.b ** 2) + (self.a ** 2):
+            return True
+        else:
+            return False
+
+    def semelhantes(self,triangulo):
+        if self.tipo_lado() == triangulo.tipo_lado():
+            return True
+        else:
+            return False
+
+
+
+
