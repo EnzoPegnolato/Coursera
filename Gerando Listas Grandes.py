1208c0e50cc951c8f1c6f124ffb460f2811f2b77
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-19 16:17:53 -0300

    Update Gerando Listas Grandes.py

--- Gerando Listas Grandes.py
+++ Gerando Listas Grandes.py
@@ -1,3 +1,7 @@
+"""Função lista_grande(n), que recebe como parâmetro um número 
+inteiro n e devolve uma lista contendo n números inteiros aleatórios."""
+
+
 import random
 def  lista_grande(n):
     lista = []
