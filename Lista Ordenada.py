e8ebca9da6b8845b0681a3c94b367ee1191dce9e
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-19 18:27:54 -0300

    Update Lista Ordenada.py

--- Lista Ordenada.py
+++ Lista Ordenada.py
@@ -1,3 +1,7 @@
+"""A função ordenada(lista) recebe uma lista com números inteiros como parâmetro
+e devolve o booleano True se a lista estiver ordenada e False se a lista não estiver ordenada."""
+
+
 def  ordenada(lista):
     x = sorted(lista)
     a = 0
