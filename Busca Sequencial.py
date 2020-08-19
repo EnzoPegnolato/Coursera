e8b115d0e88052e6ab0873933632e15b690e3313
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-19 18:57:35 -0300

    Update Busca Sequencial.py

--- Busca Sequencial.py
+++ Busca Sequencial.py
@@ -1,3 +1,7 @@
+""" A função busca(lista, elemento) busca um determinado elemento em
+uma lista e devolve o índice correspondente à posição do elemento encontrado.
+Nos casos em que o elemento buscado não existir na lista, a função deve devolver o booleano False. Sem o uso do método Index"""
+
 def busca(lista, elemento):
     b = 0
     for i in lista:
