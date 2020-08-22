9657ae6a48828a347ddfa7d633268d1f0434c455
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-22 18:20:45 -0300

    Update Fatorial  Recursivo.py

--- Fatorial  Recursivo.py
+++ Fatorial  Recursivo.py
@@ -1,3 +1,6 @@
+"""A função fatorial(x) recebe como parâmetro um número inteiro e devolve, 
+utilizando recursividade, um número inteiro correspondente ao fatorial de x."""
+
 def fatorial(x):
     if x == 0:
         return 1
