1a3671cbb56ad48669ffb5049a168a31d1603018
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-22 18:12:27 -0300

    Update Fibonacci Recursivo.py

--- Fibonacci Recursivo.py
+++ Fibonacci Recursivo.py
@@ -1,3 +1,7 @@
+"""A função fibonacci(n) recebe como parâmetro um número inteiro e devolve, utilizando 
+recursividade, um número inteiro correspondente ao n-ésimo elemento da sequência de Fibonacci."""
+
+
 def fibonacci(n):
     if n < 2:
         return n
