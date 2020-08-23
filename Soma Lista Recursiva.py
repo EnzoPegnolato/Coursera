5fa7ba1b9fb3338e73d6b82a84889f01b6f19552
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-08-23 12:38:50 -0300

    Update Soma Lista Recursiva.py

--- Soma Lista Recursiva.py
+++ Soma Lista Recursiva.py
@@ -1,3 +1,7 @@
+"""A função soma_lista(lista) recebe como parâmetro uma lista de números inteiros e devolve,
+usando recursão, um número inteiro correspondente à soma dos elementos desta lista."""
+
+
 def soma_lista(lista):
     if len(lista) == 1:
         return lista[0]
